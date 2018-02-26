f4ce5f1e981fbabfae9fbd482e2de02f97cbe280
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2018-02-25 22:57:53 -0500

    added reveal chat and channel customizations

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,33 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Shameiz Rangwala on 2018-02-25.
+//  Copyright © 2018 CodeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //IBOUTLETS
+    
+    
+    @IBOutlet weak var hamburger: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hamburger.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
---
+++ Supporting Files/SWReveal-bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  SWReveal-bridging-header.h
+//  Smack
+//
+//  Created by Shameiz Rangwala on 2018-02-25.
+//  Copyright © 2018 CodeSchool. All rights reserved.
+//
+
+#ifndef SWReveal_bridging_header_h
+#define SWReveal_bridging_header_h
+#import "SWRevealViewController.h"
+
+#endif /* SWReveal_bridging_header_h */
